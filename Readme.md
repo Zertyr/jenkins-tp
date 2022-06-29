495b4fef1bef59ea10f20b2cac70a63800f33cd4
Damien <Damino95@gmail.com>
2022-06-29 13:14:02 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,35 @@
+    Quels tests peut-on faire pour s'assurer que le code fonctionne ?
+    - PERDRE : mettre que des fausses lettres
+    - GAGNE : mettre que les bonnes lettres
+    - si on entre plus d'une lettre, message et le tour ne compte pas
+
+    1/ Si le code n'est pas dans une fonction, on ne peut pas le tester
+    2/ Il faut séparer la définition du programme de son exécution
+    3/ Il ne faut pas intégrer les interaction utilisateurs directement au sein des fonctions
+    4/ Ne pas mettre dans les fonctions de test du code applicatif
+
+
+
+
+## Intégration de jasmine
+
+Installation de jasmine : npm install jasmine
+
+Configuration initiale : npx jasmine init
+
+## Intégration de cucumber
+
+```
+npm install --save-dev @cucumber/cucumber
+mkdir features
+mkdir features/step_definitions
+``` 
+
+On crée un cucumber.js
+
+On crée la coque des step defs : `features/step_definitions/stepdefs.js`
+
+```js
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+```
---
+++ pendu.js
@@ -0,0 +1,72 @@
+exports.initGame = function(word) {
+    return {
+        mot: word,
+        motLettres: {
+            "a": false,
+            "b": false,
+            "r": false,
+            "i": false,
+            "c": false,
+            "o": false,
+            "t": false
+        },
+        lettresTrouvees: 0,
+        compteurCoupFaux: 0,
+        compteurCoupMax: 10,
+        partieFinie: false, // true si la partie est terminée
+        aGagne: false // valable uniquement lorsque partieFinie = true
+    };    
+}
+exports.pendu = function() {
+
+    var gameStatus = initGame("abricot");
+
+    while(mot.length != lettresTrouvees){
+        let userLetter = prompt('Donner une lettre')
+        entrerUneLettre(userLetter,gameStatus);
+        console.log(`Lettres trouvées : ${lettresTrouvees}`)
+    }
+
+    if(mot.length == lettresTrouvees){
+        console.log("Vous avez bien trouvé le mot")
+        console.log(`Le mot était ${mot}`)
+    }
+}
+
+exports.checkPartieTerminee = function(gameStatus) {
+    if (gameStatus.compteurCoupFaux === gameStatus.compteurCoupMax) {
+        console.log('Partie terminée');
+        gameStatus.partieFinie = true;
+        gameStatus.aGagne = false;
+        return true;
+    }
+    let aGagne = true;
+    for(let letter in gameStatus.motLettres) {
+        if(!gameStatus.motLettres[letter]) {
+            aGagne = false;
+            break;
+        }
+    }
+    if(aGagne) {
+        gameStatus.partieFinie = true;
+        gameStatus.aGagne = true;
+    }
+    return false;
+}
+
+exports.entrerUneLettre = function(userLetter,gameStatus) {
+    if (typeof userLetter === "string" && userLetter.length === 1) {
+        if(gameStatus.mot.includes(userLetter) && gameStatus.motLettres[userLetter] != true){
+            gameStatus.lettresTrouvees++
+            gameStatus.motLettres[userLetter] = true
+        } else {
+            // La lettre n'est pas dans le mot
+            gameStatus.compteurCoupFaux++;
+        }
+        exports.checkPartieTerminee(gameStatus);
+    } else {
+        console.log('Une erreur d\'entrée à été détectée sur le tour:', userLetter);
+    }
+    return gameStatus;
+}
+
